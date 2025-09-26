02416da89eab19fc9810f2fe62d8e30f7cc55464
Artem Nizamiiev <upuyayua@gmail.com>
2025-09-26 20:57:35 +0300

    Solved all problems

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from typing import Callable
 from typing import Any
+from functools import wraps
 
 
 def cache(func: Callable) -> Callable:
     cached_data = {}
 
-    def wrapper(*args) -> Any:
-        if args not in cached_data.keys():
-            cached_data[args] = func(*args)
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> Any:
+        key = (args, tuple(sorted(kwargs.items())))
+        if key not in cached_data.keys():
+            cached_data[key] = func(*args, **kwargs)
             print("Calculating new result")
         else:
             print("Getting from cache")
-        return cached_data[args]
+        return cached_data[key]
     return wrapper
